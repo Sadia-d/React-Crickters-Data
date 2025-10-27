103b3a7abffffd1a0e4cf744daed7205d5443195
Sadia-d <subai>
2025-10-27 19:43:39 +0600

    added-feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,20 @@ function App() {
   const [crickters , setCrickters] = useState([])
 
   const handleFavCrickters =(crickter)=>{
+
+    const alreadyAdd = crickters.find((c)=> c.name === crickter.name)
+    if(alreadyAdd){
+      alert('Already added')
+      return
+    }
+
     setCrickters(prev => [...prev , crickter])
   }
 
 
   return (
     <>
-   <div className='flex justify-between gap-2 px-6 mx-auto'>
+   <div className='flex justify-between gap-2 px-6 mx-auto mt-10'>
     <div>
         <Crickters handleFavCrickters={handleFavCrickters}></Crickters>
     </div>
--- src/Components/Crickter.jsx
+++ src/Components/Crickter.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Crickter = ({crickter,handleFavCrickters}) => {
-    console.log(crickter)
     const{name , runs ,team ,centuries ,fifties} = crickter;
+    
     return (        
         <div className='border-2 border-blue-400 rounded-xl mb-3 px-3
         mx-2'>
@@ -11,7 +11,7 @@ const Crickter = ({crickter,handleFavCrickters}) => {
             <p className='text-lg font-medium text-gray-500'>Team : {team} </p>
             <p className='text-lg font-medium text-gray-500'>Centuries : {centuries} </p>
             <p className='text-lg font-medium text-gray-500'>Fifties : {fifties} </p>
-            <button onClick={()=>handleFavCrickters(crickter)} className='bg-blue-600 px-4 rounded-xl  text-white mt-2 mb-2 py-2 mb-2'>Add to fav crickter</button>
+            <button onClick={()=>handleFavCrickters(crickter)} className='bg-blue-600 px-4 rounded-xl  text-white mt-2 mb-2 py-2 mb-2'>Add crickter</button>
         </div>
     
         
